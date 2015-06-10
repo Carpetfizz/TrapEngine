30d4dfc057c83f3cfbe0288fb3df7631ddd2aa33
Manoj Vasishta <mhvasishta@gmail.com>
2015-06-09 23:37:11 -0700

    +Added ctor chaining +Backported vector math operations

--- engine/src/com/ajayramesh/TrapEngine/Vector2d.java
+++ engine/src/com/ajayramesh/TrapEngine/Vector2d.java
@@ -1,11 +1,16 @@
 package com.ajayramesh.TrapEngine;
 
-public class Vector2d
+public class Vector2D
 {
-    private double x = 0.0D;
-    private double y = 0.0D;
+    private double x;
+    private double y;
 
-    public Vector2d(double x, double y)
+    public Vector2D()
+    {
+        this(0.0,0.0);
+    }
+
+    public Vector2D(double x, double y)
     {
         this.x = x;
         this.y = y;
@@ -29,5 +34,25 @@ public class Vector2d
     public void setY(double y)
     {
         this.y = y;
+    }
+
+    public double magnitude()
+    {
+        return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
+
+    public double angle()
+    {
+        return Math.atan2(this.y, this.x);
+    }
+
+    public Vector2D scale(double scalar)
+    {
+        return new Vector2D(this.x * scalar, this.y * scalar);
+    }
+
+    public Vector2D add(Vector2D v2d)
+    {
+        return new Vector2D(this.x + v2d.x, this.y + v2d.y);
     }
 }
