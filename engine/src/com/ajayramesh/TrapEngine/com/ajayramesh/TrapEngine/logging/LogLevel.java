dc7d3ca47d1a92615ac7a8b59727e5e25c8780d9
Manoj Vasishta <mhvasishta@gmail.com>
2015-06-11 13:26:03 -0700

    +LogLevel defines urgency of message, will be displayed in all messages +LogSystem defines loggable subsystems

---
+++ engine/src/com/ajayramesh/TrapEngine/com/ajayramesh/TrapEngine/logging/LogLevel.java
@@ -0,0 +1,30 @@
+package com.ajayramesh.TrapEngine.com.ajayramesh.TrapEngine.logging;
+
+
+public class LogLevel
+{
+    public static LogLevel DEBUG = new LogLevel("Debug", 0);
+    public static LogLevel INFO = new LogLevel("Info", 1);
+    public static LogLevel WARNING = new LogLevel("Warning", 2);
+    public static LogLevel ERROR = new LogLevel("Error", 3);
+
+    private String name;
+
+    private int priority;
+
+    private LogLevel(String name, int priority)
+    {
+        this.name = name;
+        this.priority = priority;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getPriority()
+    {
+        return priority;
+    }
+}
---
+++ engine/src/com/ajayramesh/TrapEngine/com/ajayramesh/TrapEngine/logging/LogSystem.java
@@ -0,0 +1,17 @@
+package com.ajayramesh.TrapEngine.com.ajayramesh.TrapEngine.logging;
+
+/**
+ * Created by Manoj on 6/11/2015.
+ */
+
+/*  Easy way to reference various engine subsystems needing to be logged
+    May need to be revamped if it gets too unwieldy
+    Unused for now
+ */
+public enum LogSystem {
+    RENDERING,
+    NETWORKING,
+    INPUT,
+    ENGINE,
+    LOGGING
+}
